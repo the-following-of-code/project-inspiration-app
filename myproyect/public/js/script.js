f0f8aad91d0412f731d0b042bf2cf9d777642724
Valentin Paul <valentin@vamptech.de>
2022-04-29 17:54:03 +0200

    initial setup with ironlauncher

---
+++ myproyect/public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("myproyect JS imported successfully!");
+  },
+  false
+);
---
+++ myproyect/utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/myproyect";
+
+module.exports = MONGO_URI;
