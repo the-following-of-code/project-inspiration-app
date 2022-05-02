5a4b23e888d6ed23cde0f2c61e178325895b2dec
Valentin Paul <valentin@vamptech.de>
2022-05-02 19:02:19 +0200

    displaying all users on homepage + linking them to the visitors page

--- myproyect/routes/index.routes.js
+++ myproyect/routes/index.routes.js
@@ -1,13 +1,28 @@
+const router = require("express").Router();
 const Book = require("../models/Books.model");
 const mongoose = require("mongoose");
+const User = require("../models/User.model");
 
-const router = require("express").Router();
 
 /* GET home page */
-router.get("/home", (req, res, next) => {
+router.get("/home", (req, res, next)=>{
+  User.find()
+  .populate("books")
+  .then((usersArr)=>{
+    console.log(usersArr);
+    // res.send("Hello");
+    res.render("home", {users: usersArr});
+  })
+  .catch(error => {
+    console.log("error displaying Books in HomePage", error);
+    next(error);
+  });
+    
+})
+// router.get("/home", (req, res, next) => {
 
-  res.render("home");
-});
+//   res.render("home");
+// });
 
 router.get("/home/user", (req, res, next)=>{
   res.render("user/user-profile", req.session.user)
@@ -23,21 +38,54 @@ router.get("/home/user/edit/user-create", (req, res, next)=>{
 })
 
 
+// router.post("/home/user/edit/user-create", (req, res, next)=>{
+//   let newBook = {
+//     title: req.body.title,
+//     author: req.body.author,
+//     cover: req.body.cover
+//   }
+// Book.create(newBook)
+// .then((book)=>{
+//   return book;
+ 
+//   // return req.session.user.books.push(book.id);
+// })
+// .then(()=>{
+//   return User.findById(req.session.user._id)
+//     .then((user)=>{
+//       console.log(user);
+//       return;
+//     })
+//   })
+//   .catch()
+//   // console.log(req.session.user);
+//   // res.redirect("/home/user")
+// })
+// .catch(error => {
+//   console.log("error creating book in DB", error);
+//   next(error);
+// });
+
 router.post("/home/user/edit/user-create", (req, res, next)=>{
   let newBook = {
-    title: req.body.title,
-    author: req.body.author,
-    cover: req.body.cover
-  }
-Book.create(newBook)
-.then((book)=>{
-  req.session.user.books.push(book.id);
+        title: req.body.title,
+        author: req.body.author,
+        cover: req.body.cover
+      }
+let bookId;
+  Book.create(newBook)
+  .then((book)=>{
+    bookId = book._id;
+    return User.findByIdAndUpdate(req.session.user._id, {$push: {books: bookId}})
+  })
+  .then(user=>{
   res.redirect("/home/user")
+  })
+  .catch()
 })
-.catch(error => {
-  console.log("error creating book in DB", error);
-  next(error);
-});
-})
+
+
+
+
 
 module.exports = router;
